16ab3f732f2d303b5ff0f22fc3fd060c3277db18
Anjali Talane <talneanjali@gmail.com>
2026-07-17 17:25:52 +0530

    Add files via upload

---
+++ React/Child.jsx
@@ -0,0 +1,9 @@
+import rract from "react";
+export default function Child(Props) {
+    return(
+        <>
+        <h1>This is child class of userState(parent )</h1>
+        <h2>{Props.send}</h2>
+        </>
+    )
+}
